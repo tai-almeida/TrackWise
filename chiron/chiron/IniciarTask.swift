a7b84d5eac1f430ae652c9582df932a9601d1b7b
Lari <l177154@dac.unicamp.br>
2025-04-16 15:57:57 -0300

    hex color file

---
+++ chiron/chiron/IniciarTask.swift
@@ -0,0 +1,20 @@
+//
+//  IniciarTask.swift
+//  chiron
+//
+//  Created by Aluno 33 on 16/04/25.
+//
+
+import SwiftUI
+
+struct IniciarTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IniciarTask_Previews: PreviewProvider {
+    static var previews: some View {
+        IniciarTask()
+    }
+}
---
+++ chiron/chiron/Tasks/taskStyle.swift
@@ -0,0 +1,8 @@
+//
+//  taskStyle.swift
+//  chiron
+//
+//  Created by Aluno 33 on 16/04/25.
+//
+
+import Foundation
