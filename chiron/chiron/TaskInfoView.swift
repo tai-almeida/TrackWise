cc12361fcda4754336e11162f1bf5a0cd6921ccf
Lari <l177154@dac.unicamp.br>
2025-04-16 15:59:46 -0300

    taskinfoview

--- chiron/chiron/TaskInfoView.swift
+++ chiron/chiron/TaskInfoView.swift
@@ -6,10 +6,118 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct TaskInfoView: View {
+    var task: Task
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack (alignment: .leading){
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    
+                    // bloco infos principais
+                    VStack (alignment: .leading){
+                        Text(task.title)
+                            .font(.title.bold())
+                            .padding(.bottom, 10)
+                        
+                        Text(task.location)
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        
+                        Text(task.formattedRangeTime)
+                            .padding(.bottom, 5)
+                        
+                        Text("Tempo médio: \(task.averageTime) min")
+                            .font(.subheadline)
+                    }
+                    .padding()
+                    
+                    // bloco infos secundarias
+                    VStack {
+                        HStack {
+                            Text("Data")
+                            Spacer()
+                            Text(task.formattedData)
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Categoria")
+                            Spacer()
+                            Text(task.category.rawValue.capitalized)
+                                .font(.caption)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 4)
+                                .background(task.category.color)
+                                .foregroundStyle(.white)
+                                .clipShape(Capsule())
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Dificuldade")
+                            Spacer()
+                            Text(task.difficulty.rawValue)
+                                .font(.caption)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 4)
+                                .background(task.difficulty.color)
+                                .foregroundStyle(.white)
+                                .clipShape(Capsule())
+                        }
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(hex: 0xF1ECDB))
+                    )
+                    .padding(.bottom)
+                    
+                    // bloco checklist
+                    Text("Checklist")
+                        .font(.caption)
+                        .fontWeight(.light)
+                        .padding(.horizontal)
+                    VStack (alignment: .leading){
+                        ForEach(task.checklist, id: \.self) { item in
+                            Divider()
+                            Text(item)
+                        }
+                    }
+                    .padding(.horizontal)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(hex: 0xF1ECDB))
+                    )
+                    
+                }
+                
+            }
+            Spacer()
+
+            NavigationLink(destination: IniciarTaskView()) {
+                Text("Começar Tarefa")
+                    .padding(.horizontal, 100)
+                    .padding(.vertical, 15)
+                    .background(Color(hex: 0x91A394))
+                    .foregroundStyle(.white)
+                    .cornerRadius(8)
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            
+        }
+        .background(Color(hex: 0xEFE8D8))
+    }
+}
+
+struct TaskInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskInfoView(task: Task.exampleTask)
     }
 }
