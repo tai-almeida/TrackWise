bdb9fc7113cc7938a4b263d60039db6ce1b7b2a0
Lari <l177154@dac.unicamp.br>
2025-04-16 09:14:58 -0300

    task file

---
+++ chiron/chiron/TaskInfoView.swift
@@ -0,0 +1,15 @@
+//
+//  TarefaInfoView.swift
+//  chiron
+//
+//  Created by Laris on 15/04/25.
+//
+
+import Foundation
+
+struct TaskInfoView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ chiron/chiron/Tasks/Task.swift
@@ -0,0 +1,50 @@
+//
+//  example_task.swift
+//  chiron
+//
+//  Created by Laris on 15/04/25.
+//
+
+import Foundation
+
+// CaseIterable permite acessar os valores como lista,
+// necessario para criar a seleçao na criaçao de Task
+enum Difficulty: String, CaseIterable, Identifiable {
+    case facil = "Fácil"
+    case medio = "Média"
+    case dificil = "Difícil"
+    
+    var id: String { self.rawValue } // rawValue pega o valor (a string)
+}
+
+enum Category: String, CaseIterable, Identifiable {
+    case estudos, lazer, faxina, social
+    
+    var id: String { self.rawValue }
+}
+
+struct Task: Identifiable {
+    let id: Int
+    var title: String
+    var location: String
+    var date: Date
+    var startTime: Date
+    var endTime: Date
+    var category: Category
+    var difficulty: Difficulty
+    var checklist: [String]
+    var isCompleted: Bool
+    
+    static let exampleTask = Task(
+        id: 0,
+        title: "Estudar Cálculo 1",
+        location: "Biblioteca Central",
+        date: Date(),
+        startTime: Date(),
+        endTime: Date().addingTimeInterval(3600),
+        category: .estudos,
+        difficulty: .dificil,
+        checklist: ["Assistir aula", "Fazer exercícios", "Revisar"],
+        isCompleted: false
+    )
+}
