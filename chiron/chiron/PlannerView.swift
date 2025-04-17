c3b3636b6fe5d316a8989aad2039d4a0da58eeac
tai-almeida <taina_bastosalmeida@hotmail.com>
2025-04-17 11:56:22 -0300

    Update PlannerView.swift

--- chiron/chiron/PlannerView.swift
+++ chiron/chiron/PlannerView.swift
@@ -13,23 +13,59 @@ struct PlannerView: View {
     
     // by default, the first tab selected is the planner (main frame)
     @State var selectedTab: Tabs = .planner
+    var week: Week
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Tela do Planner")
-                    .padding()
-                NavigationLink.init("Clique aqui", destination: Text("Proxima Tela").navigationTitle("Boas vindas a nova tela!"))
+        ZStack {
+            screenColor.ignoresSafeArea()
+            
+            NavigationView {
                 
-                Spacer()
-                
-                //TabBar()
-            }.navigationTitle("Planner")
+                VStack(alignment: .leading) {
+                    
+                    // tela scrolavel com os dias da semana
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            Text("Hoje")
+                                .font(.title.bold())
+                                .padding(.bottom, 2)
+                                .padding(.trailing, 277)
+                                .multilineTextAlignment(.leading)
+                                
+                            
+                            Text("Segunda-Feira, 10 de abril")
+                                .font(.body.bold())
+                                //.padding(.top, 1)
+                                //.padding(.leading, 5)
+                                .multilineTextAlignment(.leading)
+                            
+                            // impressao de cada evento do dia
+                            VStack (alignment: .leading){
+                                ForEach(week.events, id: \.self) { item in
+                                    Divider()
+                                    Text(item)
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                    
+                }.navigationTitle("Minha Rotina").navigationBarTitleDisplayMode(.inline)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(screenColor)
+                    
+            }
+            
+            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(screenColor)
+            .padding()
         }
     }
     
     struct PlannerView_Previews: PreviewProvider {
         static var previews: some View {
-            PlannerView()
+            PlannerView(week: Week.exampleWeek)
         }
     }
 }
