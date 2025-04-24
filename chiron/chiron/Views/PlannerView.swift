572b96df1dbd70bb8d7093f614ea4834f6b0c1da
Aluno 45 <aluno45@10-40-59-196.0200.ecps.br.iosda.org>
2025-04-24 10:00:45 -0300

    organizacao PlannerView

--- chiron/chiron/Views/PlannerView.swift
+++ chiron/chiron/Views/PlannerView.swift
@@ -5,14 +5,10 @@
 //  Created by Aluno 41 on 15/04/25.
 //
 
-import Foundation
 import SwiftUI
 
-
 struct PlannerView: View {
-    
     // by default, the first tab selected is the planner (main frame)
-    //@State var selectedTab: Tabs = .planner
     var week: Week
     var body: some View {
         ZStack {
@@ -25,34 +21,29 @@ struct PlannerView: View {
                     // tela scrolavel com os dias da semana
                     ScrollView {
                         VStack(alignment: .leading) {
+                            // "Hoje" com destaque na tela
                             Text("Hoje")
                                 .font(.title.bold())
                                 .padding(.bottom, 2)
                                 .padding(.trailing, 277)
                                 .multilineTextAlignment(.leading)
                                 
-                            
+                            // imprime o dia da semana e a data abaixo
                             Text("Segunda-Feira, 10 de abril")
                                 .font(.body.bold())
-                                //.padding(.top, 1)
-                                //.padding(.leading, 5)
                                 .multilineTextAlignment(.leading)
                             
                             // impressao de cada evento do dia
                             VStack (alignment: .leading){
                                 
+                                // cada evento separado por um divisor
                                 ForEach(week.events, id: \.self) { item in
                                     Divider()
                                         .padding(.horizontal, 20)
-                                    //Rectangle()
-                                        //.foregroundColor(Color(hex: 0xFCFFEF))
-                                        //.cornerRadius(10)
-                                        //.padding(.leading, 20)
-                                    //.padding(.trailing, 20)
+                                
                                     Text(item)
                                         .padding(.horizontal, 20)
                                     
-                                        
                                 }
                             }
                             
@@ -66,6 +57,7 @@ struct PlannerView: View {
                                 Section {
                                     ForEach(week.tasks, id: \.id) { task in
                                         HStack {
+                                            // cada tarefa separada por um divisor abaixo do horario setado (formatado)
                                             Spacer()
                                             Text(task.formattedTime)
                                                 .padding(.trailing, 10)
@@ -73,8 +65,6 @@ struct PlannerView: View {
                                         }
                                         
                                         Divider()
-                                            //.padding(.horizontal, 10)
-                                        
                                         
                                         NavigationLink(destination: TaskInfoView(task: Task.exampleTask)) {
                                             HStack {
@@ -83,12 +73,13 @@ struct PlannerView: View {
                                                     .frame(width:10)
                                                 
                                                 VStack(alignment: .leading) {
+                                                    // nome da tarefa
                                                     Text(task.title)
-                                                        //.padding(.horizontal, 100)
                                                         .padding(.top, 10)
                                                         .foregroundStyle(.black)
                                                         .padding(.leading, 5)
                                                     
+                                                    // tag de dificuldade
                                                     Text(task.difficulty.rawValue)
                                                         .font(.system(size:12))
                                                         .padding(.horizontal, 10)
@@ -100,15 +91,15 @@ struct PlannerView: View {
                                                 .padding(.bottom, 5)
                                                 Spacer()
                                                 VStack(alignment: .trailing) {
+                                                    // simbolo de inicio da tarefa
                                                     Image(systemName: "play.fill")
-                                                        //.font(.system(size:12))
                                                         .padding(.horizontal, 10)
                                                         .padding(.vertical, 4)
                                                         .background(Color("AccentColor"))
                                                         .foregroundStyle(.white)
                                                         .clipShape(Capsule())
                                                     
-                                                    //let duration: Int = task.averageTime
+                                                    // tag com o tempo convertido para HhMIN
                                                     Text(convertsTime(duration: task.averageTime))
                                                         .font(.system(size:12))
                                                         .padding(.horizontal, 10)
@@ -125,21 +116,15 @@ struct PlannerView: View {
                                         .cornerRadius(8)
                                         .padding(.horizontal, 5)
                                     }
-                                    //.padding(.horizontal, 10)
                                 }
                             }
-                                
                         }
                     }
-                    
-                    
-                }.navigationTitle("Minha Rotina").navigationBarTitleDisplayMode(.inline)
+                }.navigationTitle("Minha Rotina").navigationBarTitleDisplayMode(.inline) //titulo da tela
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color("BackgroundScreenColor"))
                     
             }
-            
-            
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("BackgroundScreenColor"))
             .padding()
